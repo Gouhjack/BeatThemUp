824101d3f3d5b4bb78b2d940f3868c58b3975119
Gouhjack <122753982+Gouhjack@users.noreply.github.com>
2023-02-06 16:22:19 +0100

    Control Player by script jump is missing

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -7,13 +7,20 @@ public class PlayerJump : MonoBehaviour
     #region Exposed
 
     [SerializeField] AnimationCurve _jumpCurve;
+    [SerializeField] float _jumpHeight = 3f;
+    [SerializeField] float _jumpDuration = 3f;
+    [SerializeField] Animator _animator;
+    [SerializeField] float _moveSpeed = 3f;
+    [SerializeField] int _maxJump = 2;
+   
 
     #endregion
 
     #region Unity LifeCycle
     private void Awake()
     {
-        _graphics = transform.Find("Graphics");
+        _graphics = transform.Find("GraphicsP1");
+        _rb2D = GetComponent<Rigidbody2D>();
     }
 
     void Start()
@@ -23,33 +30,67 @@ public class PlayerJump : MonoBehaviour
 
     void Update()
     {
-        
-        if (_jumpTimer < 1)
+        _direction.x = Input.GetAxisRaw("Horizontal") * _moveSpeed;
+        _direction.y = Input.GetAxisRaw("Vertical") * _moveSpeed;
+
+        _animator.SetFloat("moveSpeedX", Mathf.Abs(_direction.x));
+
+        if (Input.GetButtonDown("Jump"))
         {
-            _jumpTimer += Time.deltaTime;
+            _isJumping = true;
+            if (_jumpTimer < _jumpDuration)
+            {
+                _jumpTimer += Time.deltaTime;
 
-            float y = _jumpCurve.Evaluate(_jumpTimer);
+                //progression / maximum
+                float y = _jumpCurve.Evaluate(_jumpTimer / _jumpDuration);
 
-            _graphics.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+                _graphics.localPosition = new Vector3(_graphics.localPosition.x, y * _jumpHeight, _graphics.localPosition.z);
 
-            Debug.Log(y);
+            }
+            else
+            {
+                _jumpTimer = 0;
+            }
+            _animator.SetFloat("moveSpeedY", _direction.y);
+
         }
+        
+
+
+
 
     }
     private void FixedUpdate()
     {
-        
+        _rb2D.velocity = _direction * Time.fixedDeltaTime * 50;
+        //_direction.y = _rb2D.velocity.y;
+
+        if (_direction.x < 0f)
+        {
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        else if (_direction.x > 0f)
+        {
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
     }
     #endregion
 
     #region Methods
 
     Transform _graphics;
-    float _jumpTimer;
+    float _jumpTimer = 0f;
 
     #endregion
 
     #region Private & Protected
+
+    private Rigidbody2D _rb2D;
+    private Vector2 _direction;
+    private bool _isJumping;
+    private int _jumpNumber;
 
     #endregion
 }
